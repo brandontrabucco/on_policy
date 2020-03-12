ed4405555153e3f500a85ce4d0487ce06bf66f1b
Brandon Trabucco <brandon@btrabucco.com>
2020-03-12 01:25:10 -0700

    added hyperparameter tuner

--- on_policy/core/launch.py
+++ on_policy/core/launch.py
@@ -1,6 +1,10 @@
 from on_policy import init_process
 from copy import deepcopy
 import multiprocessing
+import datetime
+import os
+import tensorflow as tf
+import pickle as pkl
 
 
 def launch(baseline,
@@ -34,24 +38,50 @@ def launch(baseline,
 
     # if only one seed; then run in the main thread
     if num_seeds == 1:
+
+        # modify the path to be unique using the local time
+        date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        variant["logging_dir"] = os.path.join(
+            variant["logging_dir"], "{}/".format(date))
+
+        # save the hyper parameters used to the disk for safe keeping
+        tf.io.gfile.makedirs(variant["logging_dir"])
+        with tf.io.gfile.GFile(os.path.join(
+                variant["logging_dir"], 'variant.pkl'), "wb") as f:
+            pkl.dump(variant, f)
+
+        # run the experiment in the current process
         return baseline(variant, env)
 
     # launch the experiments on the local machine
     processes = []
     for seed in range(num_seeds):
+
+        # modify the path to be unique using the local time
         seed_variant = deepcopy(variant)
-        seed_variant["logging_dir"] += "{}/".format(seed)
+        date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        seed_variant["logging_dir"] = os.path.join(
+            seed_variant["logging_dir"], "{}/{}/".format(seed, date))
+
+        # save the hyper parameters used to the disk for safe keeping
+        tf.io.gfile.makedirs(seed_variant["logging_dir"])
+        with tf.io.gfile.GFile(os.path.join(
+                seed_variant["logging_dir"], 'variant.pkl'), "wb") as f:
+            pkl.dump(seed_variant, f)
+
+        # create a process for running the experiment
         processes.append(
             multiprocessing.Process(
                 target=baseline,
                 args=(seed_variant, env)))
 
-    # start running the experiments
+    # start running the experiments and wait for all to finish
     for p in processes:
         p.start()
 
         if not parallel:
             p.join()
+
     if parallel:
         for p in processes:
             p.join()
---
+++ on_policy/tune.py
@@ -0,0 +1,89 @@
+import itertools
+import copy
+
+
+def merge_dicts(a,
+                b,
+                path=None,
+                ignore_duplicate=False):
+    """Recursively merge to dictionaries that may have different keys
+    by creating a new merged dictionary
+
+    Arguments:
+
+    a: dict
+        a dictionary that may have nested dictionaries inside
+        not required to have the same keys as b
+    b: dict
+        a dictionary that may have nested dictionaries inside
+        not required to have the same keys as a
+    path: list
+        a list of the dictionary keys encountered while traversing
+        a nested structure of dictionaries
+    ignore_duplicate: bool
+        ignore if keys are duplicated in both a and b while
+        the values are different
+
+    Returns:
+
+    result: dict
+        a dictionary that contains all elements in a and all elements
+        in b including recursively contained dicts"""
+
+    if path is None:
+        path = []
+
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge_dicts(a[key],
+                            b[key],
+                            path + [str(key)],
+                            ignore_duplicate=ignore_duplicate)
+
+            elif a[key] == b[key]:
+                print("Same value for key: {}".format(key))
+
+            else:
+                dup = '.'.join(path + [str(key)])
+
+                if ignore_duplicate:
+                    print("duplicate key ignored: {}".format(dup))
+
+                else:
+                    raise Exception('Duplicate keys at {}'.format(dup))
+
+        else:
+            a[key] = b[key]
+
+    return a
+
+
+def grid_search(search_space,
+                variant):
+    """Creates a list of variants for performing as hyper parameter sweep
+    using the provided search space grid
+
+    Arguments:
+
+    search_space: dict
+        a dictionary containing lists of search space points to
+        perform a grid search over
+    variant: dict
+        a dictionary of hyper parameters that control the
+        rl algorithm
+
+    Returns:
+
+    variants: list
+        a list of dictionary variants of the same structure as variant
+        but different combinations of elements in search_space"""
+
+    named_h = []
+    for name, values in search_space.items():
+        named_h.append([(name, v) for v in values])
+
+    return [merge_dicts(dict(h),
+                        copy.deepcopy(variant),
+                        ignore_duplicate=True)
+            for h in itertools.product(*named_h)]
